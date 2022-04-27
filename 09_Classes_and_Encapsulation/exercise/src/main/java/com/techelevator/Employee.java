b9bf4dcb080c2b8257731fa6da6bd05629e57cfa
Mohammed Al kubati <99111288+Moepray@users.noreply.github.com>
2022-04-27 18:36:50 -0500

    module 1 commit

---
+++ 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Employee.java
@@ -0,0 +1,56 @@
+package com.techelevator;
+
+public class Employee {
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String department;
+    private double annualSalary;
+
+    public Employee(int employeeId, String firstName, String lastName, double annualSalary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.annualSalary = annualSalary;
+    }
+    public void raiseSalary (double percent) {
+        if ( percent >0.0){
+            annualSalary = ((100 + percent)/ 100.0) * annualSalary;
+        }
+
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        fullName = lastName +", " + firstName;
+        return fullName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
